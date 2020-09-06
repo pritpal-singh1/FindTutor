396540289bf5488c4d022108cfcf3f005d18788c
pritpal-singh1 <pritpaldabb@gmail.com>
2020-09-06 23:09:23 +0530

    6 sep Commit

--- app/src/main/java/com/example/findtutor/MainActivity.java
+++ app/src/main/java/com/example/findtutor/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.findtutor;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
 
+import android.graphics.Color;
 import android.os.Bundle;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+
+
     }
+
 }
